fd5ac7f734cffc68938d38b5e5fe001ceb0c6869
qkfskan82 <qkfskan82@naver.com>
2021-03-09 22:20:15 +0900

    0309

--- Backjun/q2579.java
+++ Backjun/q2579.java
@@ -5,16 +5,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class q2579 {
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int N = Integer.parseInt(br.readLine());
 		int[] stair = new int[N+1];
-		for(int n = N; n>=1; n++) {
-			stair[n] = Integer.parseInt(br.readLine());
+
+		for (int n = 0; n < N; n++) {
+			stair[n+1] = Integer.parseInt(br.readLine());
+		}
+
+		int[] dp = new int[N + 1];
+		
+		dp[1] = stair[1];
+		
+		if(N > 1) {
+			dp[2] = dp[1] + stair[2];
 		}
 		
+		for(int i = 3; i <= N; i++) {
+			dp[i] = Math.max(dp[i-2], dp[i-3] + stair[i-1]) + stair[i];
+		}
 		
+
+		System.out.println(dp[N]);
 	}
 }
