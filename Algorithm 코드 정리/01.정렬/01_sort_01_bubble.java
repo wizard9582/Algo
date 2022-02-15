9a24149503548f6909499597f7969725f752417c
wizard9582 <qkfskan82@naver.com>
2022-02-15 21:31:50 +0900

    한글 인코딩 깨짐 수정

--- Algorithm 코드 정리/01.정렬/01_sort_01_bubble.java
+++ Algorithm 코드 정리/01.정렬/01_sort_01_bubble.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class 01_sort_01_bubble {
 	static int size = 100;
 	static int bound = 1000;
-	// �������� ������ ���� ����
+	// 데이터의 갯수와 범위 설정
 
 	public static void main(String[] args) {
 		int[] data = new int[size];
@@ -16,12 +16,12 @@ public class 01_sort_01_bubble {
 		for (int i = 0; i < size; i++) {
 			data[i] = random.nextInt(bound);
 		}
-		// ���� �� �־��ֱ�
+		// 랜덤 값 넣어주기
 
 		System.out.println(Arrays.toString(data));
-		// �����ϰ� �� ������ Ȯ��
+		// 랜덤하게 들어간 데이터 확인
 
-		/////////////////////// �������� ����////////////////////////////
+		/////////////////////// 버블정렬 구현////////////////////////////
 		for (int find = size - 1; find >= 0; find--) {
 			for (int now = 0; now < find; now++) {
 				int next = now + 1;
@@ -36,8 +36,8 @@ public class 01_sort_01_bubble {
 		//////////////////////////////////////////////////////
 
 		System.out.println(Arrays.toString(data));
-		System.out.println("��Ƚ�� : " + count);
-		// ���� �� �����Ϳ� ��Ƚ�� Ȯ��
+		System.out.println("비교횟수 : " + count);
+		// 정렬 후 데이터와 비교횟수 확인
 
 		int[][] data2 = { { 1, 1 }, { 3, 1 }, { 5, 1 }, { 7, 1 }, { 9, 1 }, { 1, 2 }, { 2, 2 }, { 4, 2 }, { 1, 3 },
 				{ 7, 2 }, { 9, 3 }, { 3, 3 }, { 4, 3 }, { 8, 3 }, { 6, 3 } };
@@ -51,10 +51,10 @@ public class 01_sort_01_bubble {
 				}
 			}
 		}
-		
+
 		for (int i = 0; i < 15; i++) {
-			System.out.printf("���ĵ� �� : %d, stable: %d\n", data2[i][0], data2[i][1]);
+			System.out.printf("정렬된 수 : %d, stable: %d\n", data2[i][0], data2[i][1]);
 		}
-		//stable ������!
+		//stable 정렬임!
 	}
 }
