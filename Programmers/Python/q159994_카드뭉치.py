3f8e3a2976337a67413096dc7f0b5cdca36bcbb4
wizard9582 <qkfskan82@naver.com>
2023-06-20 23:12:33 +0900

    230620

--- Programmers/Python/q159994_카드뭉치.py
+++ Programmers/Python/q159994_카드뭉치.py
@@ -11,3 +11,18 @@ def solution(cards1, cards2, goal):
             break
     
     return "Yes" if f else "No"
+
+
+# solution 2
+def solution2(cards1, cards2, goal):
+    i, j = 0, 0
+    
+    for g in goal:
+        if i < len(cards1) and cards1[i] == g:
+            i += 1
+        elif j < len(cards2) and cards2[j] == g:
+            j += 1
+        else:
+            return "No"
+    
+    return "Yes"
