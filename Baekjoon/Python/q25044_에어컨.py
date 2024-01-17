f59ec3e64f5f34ed1ba4aa62ed3de382988a4db4
wizard9582 <qkfskan82@naver.com>
2024-01-17 23:48:25 +0900

    240117

--- Baekjoon/Python/q25044_에어컨.py
+++ Baekjoon/Python/q25044_에어컨.py
@@ -3,13 +3,23 @@ import sys
 n, k = sys.stdin.readline().split()
 n, k = int(n), int(k)
 
-t = [180, 180, 1080 + k]
+t = 900
+gap = [180, 180, k + 1080]
+answer = []
 
-d = 1440 * n
-g = 900
-i = 0
-while g <= d:
-    g += t[i]
-    i = (i + 1) % 3
+d, e = 1440 * n, 1440 * (n + 1)
+idx = 0
 
-print(g)
+while t < e:
+    if d <= t < e:
+        answer.append(t - d)
+    t += gap[idx]
+    idx = (idx + 1) % 3
+
+print(len(answer))
+for a in answer:
+    m = a // 60
+    d = a % 60
+    m = str(m) if m >= 10 else "0" + str(m)
+    d = str(d) if d >= 10 else "0" + str(d)
+    print(m + ":" + d)
